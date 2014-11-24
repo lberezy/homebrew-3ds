3a97bdf81cdbe920bd7466d131107b07aee1b1c6
Lucas Berezy <lucas@berezy.me>
2014-11-24 15:54:02 +1100

    Added initial formula for devkitARM.
    Completely broken, for reference only.

---
+++ devkitarm.rb
@@ -0,0 +1,23 @@
+require "formula"
+
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Devkitarm < Formula
+  homepage ""
+  url "http://sourceforge.net/p/devkitpro/perlupdaters/ci/master/tree/devkitARMupdate.pl"
+  sha1 ""
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
